0025709118ac86644fea7a23f1ca3aeffb2206d7
sirUnchained <bbehnam3831@gmail.com>
2025-02-13 15:02:43 +0330

    finished adding db to project

---
+++ src/databases/mysql_db/mysql_db.go
@@ -0,0 +1,33 @@
+package mysql_db
+
+import (
+	"arayeshyab/src/configs"
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DBClient *gorm.DB
+
+func GetDB() *gorm.DB {
+	return DBClient
+}
+
+func InitMysql(cfg *configs.Configs) {
+	conn_str := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", cfg.Mysql.Username, cfg.Mysql.Password, cfg.Mysql.Host, cfg.Mysql.Port, cfg.Mysql.Database)
+
+	// connect to db
+	var err error
+	DBClient, err = gorm.Open(mysql.Open(conn_str), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	// test db connection
+	sqldb, _ := DBClient.DB()
+	err = sqldb.Ping()
+	if err != nil {
+		panic(err)
+	}
+}
