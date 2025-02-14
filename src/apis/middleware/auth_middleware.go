53c0d30f7d6494efb35dbe333ff874cbdf882b17
sirUnchained <bbehnam3831@gmail.com>
2025-02-14 15:19:15 +0330

    added Authorize middleware

--- src/apis/middleware/auth_middleware.go
+++ src/apis/middleware/auth_middleware.go
@@ -1,8 +1,41 @@
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"arayeshyab/src/apis/helpers"
+	"arayeshyab/src/databases/mysql_db"
+	"arayeshyab/src/databases/schemas"
+	"arayeshyab/src/services"
+
+	"github.com/gin-gonic/gin"
+)
 
 func Authorize() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
+		token, err := ctx.Cookie("AccessToken")
+		if err != nil {
+			helpers.SendResult(&helpers.Result{Ok: false, Status: 401, Message: "لطفا وارد شوید", Data: nil}, ctx)
+			ctx.Abort()
+			return
+		}
+
+		ts := services.GetTokenService()
+		claims, result := ts.GetTokenClaims(token)
+		if !result.Ok {
+			helpers.SendResult(result, ctx)
+			ctx.Abort()
+			return
+		}
+
+		user := new(schemas.User)
+		db := mysql_db.GetDB()
+		db.Model(&schemas.User{}).Where("ID = ?", claims.ID).First(user)
+		if user.ID == 0 {
+			helpers.SendResult(&helpers.Result{Ok: false, Status: 400, Message: "اطلاعات شما پیدا نشد لطفا وارد شوید", Data: nil}, ctx)
+			ctx.Abort()
+			return
+		}
+
+		ctx.Set("user", user)
+		ctx.Next()
 	}
 }
